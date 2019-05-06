9a61a46ba080621f6cc615ef4d6e5cef518aaa22
Sebastian Lackner <sebastian@fds-team.de>
2019-05-06 14:43:22 +0200

    Rework the gocryptfs inspection scripts.

---
+++ gocryptfs.py
@@ -0,0 +1,172 @@
+#!/usr/bin/python3
+from Cryptodome.Protocol.KDF import HKDF
+from Cryptodome.Cipher import AES
+from Cryptodome.Hash import SHA256
+import itertools
+import argparse
+import getpass
+import hashlib
+import struct
+import base64
+import json
+import sys
+import io
+import os
+
+PLAINTEXT_ZERO = b"\x00" * 4096
+CIPHERTEXT_ZERO = b"\x00" * (4096 + 32)
+
+def decode_masterkey(masterkey):
+    return bytes.fromhex(masterkey.replace("-", ""))
+
+class GocryptfsConfig:
+    def __init__(self, filename=None, basepath=None):
+        if filename is None:
+            basepath = os.path.abspath(basepath)
+            while True:
+                filename = os.path.join(basepath, "gocryptfs.conf")
+                if os.path.isfile(filename):
+                    break
+                basepath = os.path.dirname(basepath)
+                if basepath == "/":
+                    raise RuntimeError("Failed to find gocryptfs.conf")
+
+        with open(filename, "rb") as fp:
+            self.config = json.load(fp)
+
+        # We only support file systems created with gocryptfs version >= 1.3.
+        # Older file systems did not use HKDF to derive separate keys yet.
+        assert self.config['Version'] == 2
+        assert 'HKDF' in self.config['FeatureFlags']
+
+    def get_masterkey(self, password):
+        scrypt = self.config['ScryptObject']
+        block  = base64.b64decode(self.config['EncryptedKey'])
+
+        scryptkey = hashlib.scrypt(password.encode('utf-8'),
+                                   salt=base64.b64decode(scrypt['Salt']),
+                                   n=scrypt['N'], r=scrypt['R'], p=scrypt['P'],
+                                   maxmem=0x7fffffff, dklen=scrypt['KeyLen'])
+
+        key = HKDF(scryptkey, salt=b"", key_len=32, hashmod=SHA256,
+                   context=b"AES-GCM file content encryption")
+
+        assert len(block) > 32
+        assert block != CIPHERTEXT_ZERO
+        # Layout: [ NONCE | CIPHERTEXT (...) |  TAG  ]
+        nonce, tag, ciphertext = block[:16], block[-16:], block[16:-16]
+        aes = AES.new(key, AES.MODE_GCM, nonce=nonce)
+        aes.update(struct.pack(">Q", 0))
+        return aes.decrypt_and_verify(ciphertext, tag)
+
+    @property
+    def aessiv(self):
+        return 'AESSIV' in self.config['FeatureFlags']
+
+class GocryptfsFile:
+    def __init__(self, filename, masterkey, aessiv=False):
+        if aessiv:
+            self.decrypt_block = self.decrypt_siv_block
+            self.key = HKDF(masterkey, salt=b"", key_len=64, hashmod=SHA256,
+                            context=b"AES-SIV file content encryption")
+        else:
+            self.decrypt_block = self.decrypt_gcm_block
+            self.key = HKDF(masterkey, salt=b"", key_len=32, hashmod=SHA256,
+                            context=b"AES-GCM file content encryption")
+
+        self.fp = open(filename, "rb")
+        header = self.fp.read(18)
+
+        if len(header) == 0:
+            # An empty file is valid. It means that the plaintext file
+            # was empty.
+            self.fileid = None
+        else:
+            assert len(header) == 18
+            assert header[0:2] == b"\x00\x02"
+            self.fileid  = header[2:]
+
+        self.blockno = 0
+        self.remaining = io.BytesIO()
+
+    def decrypt_gcm_block(self, block):
+        assert len(block) > 32
+        # File holes are passed through to the underlying FS.
+        if block == CIPHERTEXT_ZERO:
+            return PLAINTEXT_ZERO
+        # Layout: [ NONCE | CIPHERTEXT (...) |  TAG  ]
+        nonce, tag, ciphertext = block[:16], block[-16:], block[16:-16]
+        aes = AES.new(self.key, AES.MODE_GCM, nonce=nonce)
+        aes.update(struct.pack(">Q", self.blockno) + self.fileid)
+        return aes.decrypt_and_verify(ciphertext, tag)
+
+    def decrypt_siv_block(self, block):
+        assert len(block) > 32
+        # File holes are passed through to the underlying FS.
+        if block == CIPHERTEXT_ZERO:
+            return PLAINTEXT_ZERO
+        # Layout: [ NONCE |  TAG  | CIPHERTEXT (...) ]
+        nonce, tag, ciphertext = block[:16], block[16:32], block[32:]
+        aes = AES.new(self.key, AES.MODE_SIV, nonce=nonce)
+        aes.update(struct.pack(">Q", self.blockno) + self.fileid)
+        return aes.decrypt_and_verify(ciphertext, tag)
+
+    def fill_buffer(self):
+        self.fp.seek(18 + (4096 + 32) * self.blockno)
+        block = self.fp.read(4096 + 32)
+        if len(block) == 0:
+            self.remaining = io.BytesIO()
+            return False
+
+        self.remaining = io.BytesIO(self.decrypt_block(block))
+        self.blockno += 1
+        return True
+
+    def seek(self, offset):
+        self.blockno = offset // 4096
+        self.fill_buffer()
+        self.remaining.seek(offset % 4096)
+
+    def read(self, size=None):
+        if self.fileid is None:
+            return b""
+
+        result = io.BytesIO()
+        while size is None or size > 0:
+            data = self.remaining.read(size)
+            result.write(data)
+            if size is not None:
+                size -= len(data)
+            if size is None or size > 0:
+                if not self.fill_buffer():
+                    break
+
+        return bytes(result.getbuffer())
+
+    def close(self):
+        self.fp.close()
+        self.fp = None
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Decrypt individual file from a gocryptfs volume")
+    parser.add_argument('--aessiv', action='store_true', help="AES-SIV encryption")
+    parser.add_argument('--masterkey', type=decode_masterkey, help="Masterkey as hex string representation")
+    parser.add_argument('--password', help="Password to unlock config file")
+    parser.add_argument('--config', help="Path to gocryptfs.conf configuration file")
+    parser.add_argument('filename', help="Location of the file to decrypt")
+    args = parser.parse_args()
+
+    if args.masterkey is None:
+        config = GocryptfsConfig(filename=args.config, basepath=os.path.dirname(args.filename))
+        if args.password is None:
+            args.password = getpass.getpass('Password: ')
+        args.masterkey = config.get_masterkey(args.password)
+        args.aessiv = config.aessiv
+
+    fp = GocryptfsFile(args.filename, args.masterkey, args.aessiv)
+    while True:
+        block = fp.read(4096)
+        if len(block) == 0:
+            break
+        sys.stdout.buffer.write(block)
+    fp.close()
---
+++ tests/tests.py
@@ -0,0 +1,162 @@
+#!/usr/bin/python3
+import subprocess
+import unittest
+import tempfile
+import random
+import time
+import sys
+import os
+
+sys.path.insert(0, "../")
+import gocryptfs
+
+def generate_content():
+    blocks = []
+    for _ in range(random.randint(8, 16)):
+        if random.randint(0, 1) == 0:
+            blocks.append(os.urandom(4096))
+        else:
+            blocks.append(gocryptfs.PLAINTEXT_ZERO)
+    if random.randint(0, 1) == 0:
+        blocks.append(os.urandom(random.randint(1, 4095)))
+    content = b"".join(blocks)
+    return content
+
+def gocryptfs_encrypt(content, masterkey, aessiv):
+    """Create an encrypted file with the given content."""
+
+    cipher = tempfile.TemporaryDirectory(prefix="gocryptfs-cipher-")
+    plain = tempfile.TemporaryDirectory(prefix="gocryptfs-plain-")
+
+    # Create .diriv file, otherwise gocryptfs refuses to mount.
+    with open(os.path.join(cipher.name, "gocryptfs.diriv"), "wb") as fp:
+        fp.write(os.urandom(16))
+
+    # Mount cipher -> plain.
+    args = ["gocryptfs"]
+    args.append("-q")
+    if aessiv:
+        args.append("-aessiv")
+    args.append("-masterkey=%s" % masterkey.hex())
+    args.append("--")
+    args.append(cipher.name)
+    args.append(plain.name)
+    subprocess.check_call(args)
+
+    # Now create a test file with the content.
+    with open(os.path.join(plain.name, "testfile"), "wb") as fp:
+        hole = False
+        for pos in range(0, len(content), 4096):
+            hole = (content[pos:pos+4096] == gocryptfs.PLAINTEXT_ZERO)
+            if not hole:
+                fp.seek(pos)
+                fp.write(content[pos:pos+4096])
+        if hole:
+            fp.truncate(len(content))
+
+    subprocess.check_call(["fusermount", "-u", plain.name])
+
+    # Find the encrypted version.
+    found = []
+    for filename in os.listdir(cipher.name):
+        if filename == "gocryptfs.diriv":
+            continue
+        if filename == "gocryptfs.conf":
+            continue
+        found.append(os.path.join(cipher.name, filename))
+
+    # We expect to find exactly one file.
+    assert len(found) == 1
+    filename = found[0]
+
+    # Copy the ciphertext file.
+    encrypted = tempfile.NamedTemporaryFile()
+    with open(filename, "rb") as fp:
+        encrypted.write(fp.read())
+        encrypted.flush()
+
+    cipher.cleanup()
+    plain.cleanup()
+    return encrypted
+
+class GocryptfsConfigTests(unittest.TestCase):
+    def test_masterkey(self):
+        config = gocryptfs.GocryptfsConfig("./gocryptfs.conf")
+        self.assertFalse(config.aessiv)
+        masterkey = config.get_masterkey("test")
+        self.assertEqual(masterkey, gocryptfs.decode_masterkey("fd890dab-86bf61cf-ec5ad460-ad3ed01f-9c52d546-2a31783d-a56b088d-3d05232e"))
+
+class GocryptfsFileTests(unittest.TestCase):
+    def test_gcm_random(self):
+        masterkey = os.urandom(32)
+
+        content = generate_content()
+        file = gocryptfs_encrypt(content, masterkey, aessiv=False)
+        fp = gocryptfs.GocryptfsFile(file.name, masterkey, aessiv=False)
+        self.assertEqual(fp.read(), content)
+        for _ in range(1000):
+            offset = random.randint(0, len(content) + 10)
+            size   = random.randint(0, len(content) + 10 - offset)
+            fp.seek(offset)
+            self.assertEqual(fp.read(size), content[offset:offset+size])
+        fp.close()
+
+        result = subprocess.check_output(["../gocryptfs.py", "--masterkey=%s" % masterkey.hex(), file.name])
+        self.assertEqual(result, content)
+
+    def test_gcm_empty(self):
+        masterkey = os.urandom(32)
+
+        file = gocryptfs_encrypt(b"", masterkey, aessiv=False)
+        fp = gocryptfs.GocryptfsFile(file.name, masterkey, aessiv=False)
+        self.assertEqual(fp.read(), b"")
+        fp.seek(4096)
+        self.assertEqual(fp.read(), b"")
+        fp.close()
+
+        result = subprocess.check_output(["../gocryptfs.py", "--masterkey=%s" % masterkey.hex(), file.name])
+        self.assertEqual(result, b"")
+
+    def test_siv_random(self):
+        masterkey = os.urandom(32)
+
+        content = generate_content()
+        file = gocryptfs_encrypt(content, masterkey, aessiv=True)
+        fp = gocryptfs.GocryptfsFile(file.name, masterkey, aessiv=True)
+        self.assertEqual(fp.read(), content)
+        for _ in range(1000):
+            offset = random.randint(0, len(content) + 10)
+            size   = random.randint(0, len(content) + 10 - offset)
+            fp.seek(offset)
+            self.assertEqual(fp.read(size), content[offset:offset+size])
+        fp.close()
+
+        result = subprocess.check_output(["../gocryptfs.py", "--masterkey=%s" % masterkey.hex(), "--aessiv", file.name])
+        self.assertEqual(result, content)
+
+    def test_siv_empty(self):
+        masterkey = os.urandom(32)
+
+        file = gocryptfs_encrypt(b"", masterkey, aessiv=True)
+        fp = gocryptfs.GocryptfsFile(file.name, masterkey, aessiv=True)
+        self.assertEqual(fp.read(), b"")
+        fp.seek(4096)
+        self.assertEqual(fp.read(), b"")
+        fp.close()
+
+        result = subprocess.check_output(["../gocryptfs.py", "--masterkey=%s" % masterkey.hex(), "--aessiv", file.name])
+        self.assertEqual(result, b"")
+
+class GocryptfsTests(unittest.TestCase):
+    def test_masterkey(self):
+        result = subprocess.check_output(["../gocryptfs.py", "--masterkey=fd890dab-86bf61cf-ec5ad460-ad3ed01f-9c52d546-2a31783d-a56b088d-3d05232e", "./mGj2_hdnHe34Sp0iIQUwuw"])
+        self.assertEqual(result, b"It works!\n")
+
+    def test_config(self):
+        result = subprocess.check_output(["../gocryptfs.py", "--config=./gocryptfs.conf", "--password=test", "./mGj2_hdnHe34Sp0iIQUwuw"])
+        self.assertEqual(result, b"It works!\n")
+        result = subprocess.check_output(["../gocryptfs.py", "--password=test", "./mGj2_hdnHe34Sp0iIQUwuw"])
+        self.assertEqual(result, b"It works!\n")
+
+if __name__ == '__main__':
+    unittest.main()
